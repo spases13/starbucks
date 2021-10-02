9f972555b57c7027d49f7af97a07c456e7b47c3a
spases13 <85458127+spases13@users.noreply.github.com>
2021-10-02 15:15:44 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+const greenThumb = document.getElementById('green')
+const roseThumb = document.getElementById('rose')
+const purpleThumb = document.getElementById('purple')
+const circle = document.getElementById('circle')
+const bigThumb = document.getElementById('bigThumb')
+const bigWord = document.getElementById('bigWord')
+const LM = document.getElementById('LM')
+const loadingPage = document.getElementById('loading-page')
+
+
+
+window.addEventListener('load', setInterval(_ => {
+    loadingPage.style.display = 'none'
+}, 1000));
+
+greenThumb.addEventListener('mouseover', _ => {
+    bigThumb.innerHTML = '<img src="images/img1.png" alt="bigStarbucks" class="img1">';
+    circle.style.backgroundColor = '#296d51'
+    bigWord.style.color = '#296d51'
+    LM.style.backgroundColor = '#296d51'
+});
+roseThumb.addEventListener('mouseover', _ => {
+    bigThumb.innerHTML = '<img src="images/img2.png" alt="bigStarbucks"  class="img2">';
+    circle.style.backgroundColor = '#CDBBA7'
+    bigWord.style.color = '#CDBBA7'
+    LM.style.backgroundColor = '#CDBBA7'
+});
+purpleThumb.addEventListener('mouseover', _ => {
+    bigThumb.innerHTML = '<img src="images/img3.png" alt="bigStarbucks" class="img3">';
+    circle.style.backgroundColor = '#D62AD0'
+    bigWord.style.color = '#D62AD0'
+    LM.style.backgroundColor = '#D62AD0'
+});
+
+
